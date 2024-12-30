ff652cde237e9cbd10de251e3f2e4a15ea6ef52f
Mahadevan Reji <rmahadevan574@gmail.com>
2024-12-30 14:24:09 +0530

    Update app.json runtimeVersion policy; modify blur style in MessageItem for improved background effect

--- components/MessageItem.js
+++ components/MessageItem.js
@@ -200,11 +200,8 @@ const styles = StyleSheet.create({
   },
   blur: {
     position: "absolute",
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
     ...StyleSheet.absoluteFillObject,
+    backgroundColor: "rgba(24,26,32,0.5)",
   },
 });
 
